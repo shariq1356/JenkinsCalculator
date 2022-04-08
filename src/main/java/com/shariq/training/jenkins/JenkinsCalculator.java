1250e9cf0d54214dc2741e635ea637fc5c9779a3
Shariq <shariq1356@gmail.com>
2022-04-09 02:51:01 +0530

    Jenkins commit

---
+++ src/main/java/com/shariq/training/jenkins/JenkinsCalculator.java
@@ -0,0 +1,17 @@
+package com.shariq.training.jenkins;
+
+public class JenkinsCalculator {
+	
+	//add two numbers
+	public int addNumbers(int numberOne, int numberTwo) {
+		return numberOne + numberTwo;
+	}
+	
+	//subtract numbers
+
+	public int subtractNumbers(int numberOne, int numberTwo){
+		return numberOne - numberTwo;
+
+		}
+
+}
---
+++ src/test/java/com/shariq/training/jenkins/jenkinsCalcTest.java
@@ -0,0 +1,23 @@
+package com.shariq.training.jenkins;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class jenkinsCalcTest {
+
+	@Test
+	public void addTest(){
+		JenkinsCalculator myCalc = new JenkinsCalculator();
+		assertEquals(10, myCalc.addNumbers(5, 5));
+	}
+	
+	
+	@Test
+	public void subtractTest() {
+		JenkinsCalculator myCalc = new JenkinsCalculator();
+		assertEquals(5, myCalc.subtractNumbers(10, 5));
+		
+	}
+
+}
